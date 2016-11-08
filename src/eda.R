55c296310ea3ddf64e84d882c543d3d3985621ac
Gdsutton <gdsutton@outlook.com>
2016-11-08 09:37:27 +0000

    added random forest model fit in

--- src/eda.R
+++ src/eda.R
@@ -8,7 +8,10 @@ for (dataset in project.info$data)
 }
 
 # 560 variables
-names(X.train)
+dim(X.train)
+
+str(y.train)
+y.train <- as.factor(y.train[,1])
 
 # counts of column data types
 table(sapply(X.train, class))
@@ -17,4 +20,30 @@ summary(X.train)
 # classes are well balanced
 table(y.train)
 
+# there are a fair amount of highly correlated variables
+corr_matrix <- cor(X.train,)
+hist(corr_matrix, bins = 30)
+
 library(caret)
+library(e1071)
+library(randomForest)
+
+length(findCorrelation(corr_matrix, cutoff = 0.7))
+
+
+# fit model in caret
+train_ctrl <- trainControl(method = "boot", 
+                           number = 5, 
+                           verboseIter = TRUE,
+                           returnResamp = 'all'
+                           )
+
+train <- train(x = X.train,
+               y = y.train,
+               preProcess = c("nzv","pca"),
+               method = "rf",
+               # tuneGrid = expand.grid(),
+               trControl = train_ctrl
+               )
+
+plot(train)
